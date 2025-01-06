50997b03ed0c57a65f573695532027ba4b7bec24
Matyáš Balcar <matyasbalcar0@gmail.com>
2025-01-06 23:50:00 +0100

    small email changes

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,10 +1,10 @@
 class OrderMailer < ApplicationMailer
-  default from: 'no-reply@example.com' # Replace with your email address
+  default from: 'no-reply@example.com'
 
   def order_sent_email(order)
     @order = order
     @produkt = order.produkt
 
-    mail(to: @order.email, subject: "Your Order ##{@order.id} Has Been Sent")
+    mail(to: @order.email, subject: "Řepínci odeslali vaší objednávku číslo ##{@order.id}")
   end
 end
